c754bb49c18d63be7b9a8aa6aae6fdf871fd7d03
Ronny Siikaluoma <ronny.siikaluoma@husbanken.no>
2022-08-01 08:48:13 +0200

    Set up next.js

---
+++ pages/api/designsystems.js
@@ -0,0 +1,252 @@
+export const designsystems = [
+    {
+        name: "Altinn Designsystem",
+        company: "Altinn",
+        url: "https://altinn.github.io/designsystem-styleguide/",
+        repo: {
+            name: "Github",
+            url: "https://github.com/Altinn/DesignSystem/"
+        },
+        contains: [
+            "components"
+        ]
+    },
+    {
+        name: "DNB Eufemia",
+        company: "DNB",
+        url: "https://eufemia.dnb.no/",
+        repo: {
+            name: "Github",
+            url: "https://github.com/dnbexperience/eufemia"
+        },
+        contains: [
+            "components",
+            "designers-kit"
+        ]
+    },
+    {
+        name: "Elsa - domstolenes designsystem",
+        company: "Norges Domstoler",
+        url: "https://design.domstol.no/",
+        contains: [
+            "components",
+            "voice-and-tone",
+            "designers-kit"
+        ]
+    },
+    {
+        name: "Entur Designsystem",
+        company: "Entur",
+        url: "https://design.entur.org/",
+        contains: [
+            "components",
+            "voice-and-tone",
+            "designers-kit"
+        ]
+    },
+    {
+        name: "Equinor Design System",
+        company: "Equinor",
+        url: "https://eds.equinor.com/",
+        repo: {
+            name: "Github",
+            url: "https://github.com/equinor/design-system"
+        },
+        contains: [
+            "components",
+            "voice-and-tone",
+            "designers-kit"
+        ]
+    },
+    {
+        name: "Finnivers",
+        company: "Finn.no",
+        url: "https://finnivers.finn.no/",
+        contains: [
+            "components",
+            "voice-and-tone",
+            "designers-kit"
+        ]
+    },
+    {
+        name: "Hedwig",
+        company: "Posten/Bring",
+        url: "https://hedwig-docs.herokuapp.com/",
+        repo: {
+            name: "Github",
+            url: "https://github.com/bring/hedwig"
+        },
+        contains: [
+            "components",
+            "voice-and-tone",
+            "designers-kit"
+        ]
+    },
+    {
+        name: "Innovasjon Norge Brand",
+        company: "Innovasjon Norge",
+        url: "https://company-139306.frontify.com/d/YokG7g2Ch3dy/brand",
+        contains: [
+            "components",
+            "designers-kit"
+        ]
+    },
+    {
+        name: "Jøkul Designsystem",
+        company: "Fremtind",
+        url: "https://fremtind.github.io/jokul/",
+        repo: {
+            name: "Github",
+            url: "https://github.com/fremtind/jokul"
+        },
+        contains: [
+            "components",
+            "voice-and-tone",
+            "designers-kit"
+        ]
+    },
+    {
+        name: "Triton Design System",
+        company: "Kongsberg Digital",
+        url: "https://designsystem.kognif.ai/",
+        repo: {
+            name: "Github",
+            url: "https://github.com/fremtind/jokul"
+        },
+        contains: [
+            "components"
+        ]
+    },
+    {
+        name: "Lillestrøm Kommune Designsystem",
+        company: "Lillestrøm Kommune",
+        url: "https://design.lillestrom.kommune.no/guide/",
+        contains: [
+            "components",
+            "voice-and-tone",
+        ]
+    },
+    {
+        name: "Mattilsynet Designsystem",
+        company: "Mattilsynet Kommune",
+        url: "https://mattilsynet.github.io/designsystem/",
+        repo: {
+            name: "Github",
+            url: "https://github.com/Mattilsynet/designsystem"
+        },
+        contains: [
+            "components"
+        ]
+    },
+    {
+        name: "NAF Designsystem",
+        company: "NAF",
+        url: "https://merkevare.naf.no/",
+        contains: [
+            "components",
+            "voice-and-tone",
+            "designers-kit"
+        ]
+    },
+    {
+        name: "Nav Designsystem",
+        company: "Nav Kommune",
+        url: "https://design.nav.no/",
+        repo: {
+            name: "Github",
+            url: "https://github.com/navikt/nav-frontend-moduler"
+        },
+        contains: [
+            "components",
+            "voice-and-tone",
+            "designers-kit"
+        ]
+    },
+    {
+        name: "Norges nasjonalparker Designmanual",
+        company: "Norges nasjonalparker",
+        url: "https://designmanual.norgesnasjonalparker.no/",
+        contains: [
+            "voice-and-tone",
+            "designers-kit"
+        ]
+    },
+    {
+        name: "Norsk Tipping Designguide",
+        company: "Norsk Tipping",
+        url: "http://nt-designguide.no/",
+        contains: [
+            "designers-kit"
+        ]
+    },
+    {
+        name: "Origo",
+        company: "NRK",
+        url: "https://static.nrk.no/origo/latest/index.html",
+        contains: [
+            "components",
+            "designers-kit"
+        ]
+    },
+    {
+        name: "Ruter Design System",
+        company: "Ruter",
+        url: "https://brand.ruter.no",
+        contains: [
+            "components",
+            "voice-and-tone",
+            "designers-kit"
+        ]
+    },
+    {
+        name: "Skatteetaten Designsystem",
+        company: "Skatteetaten",
+        url: "https://skatteetaten.github.io/frontend-components/",
+        repo: {
+            name: "Github",
+            url: "https://github.com/Skatteetaten/frontend-components"
+        },
+        contains: [
+            "components",
+            "voice-and-tone",
+            "designers-kit"
+        ]
+    },
+    {
+        name: "SpareBank 1 Designsystem",
+        company: "Sparebank 1",
+        url: "https://design.sparebank1.no/",
+        repo: {
+            name: "Github",
+            url: "https://github.com/SpareBank1/designsystem"
+        },
+        contains: [
+            "components",
+            "voice-and-tone",
+            "designers-kit"
+        ]
+    },
+    {
+        name: "Spor",
+        company: "Vy",
+        url: "https://spor.cloud.vy.no/",
+        repo: {
+            name: "Github",
+            url: "https://github.com/nsbno/spor"
+        },
+        contains: [
+            "components",
+            "voice-and-tone",
+            "designers-kit"
+        ]
+    },
+    {
+        name: "Vipps Brand",
+        company: "Vipps",
+        url: "https://brand.vipps.no/",
+        contains: [
+            "voice-and-tone",
+            "designers-kit"
+        ]
+    },
+]
